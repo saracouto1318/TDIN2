26174f4d254124940792a9b82e75dc5db40553ee
saracouto1318 <saracouto1318@hotmail.com>
2018-04-30 20:25:06 +0100

    changes

--- TDIN/TDIN/Controllers/ProfileController.cs
+++ TDIN/TDIN/Controllers/ProfileController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace TDIN.Controllers
 {
